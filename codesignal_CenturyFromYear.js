5f9b2818ab1d04553f95a372d88da754d8f4099c
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-10-18 13:31:52 -0700

    Update codesignal_CenturyFromYear.js

--- codesignal_CenturyFromYear.js
+++ codesignal_CenturyFromYear.js
@@ -2,9 +2,14 @@
 // year 1 up to and including the year 100, the second - from the year 101 up to 
 // and including the year 200, etc.
 
+
+year = 1000; 
+
 function solution(year) {
     const x = year + 99
     const y = x/100
     const z = Math.floor(y)
     return z
 }
+
+console.log(solution(year));
