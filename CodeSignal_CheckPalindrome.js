bf7f5488f68de2a575b550066336bd8828b050c0
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-10-18 13:42:44 -0700

    Update CodeSignal_CheckPalindrome.js

--- CodeSignal_CheckPalindrome.js
+++ CodeSignal_CheckPalindrome.js
@@ -1,7 +1,11 @@
+// Given the string, check if it is a palindrome.
 
+inputString = 'aabaa'; 
 
 function solution(inputString) {
     const x = inputString.split('')
     const y = x.reverse()
-    return y
+    return y == x
 }
+
+console.log(solution(inputString));
